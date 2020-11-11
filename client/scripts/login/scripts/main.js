ff17dc07f1350a9a46fc44de792b3612e3358346
jaeyoungchang5 <58242673+jaeyoungchang5@users.noreply.github.com>
2020-11-11 05:00:05 -0500

    networking post delete

--- client/scripts/login/scripts/main.js
+++ client/scripts/login/scripts/main.js
@@ -20,14 +20,14 @@ deleteButton.onmouseup = function(){
 
 function getFormInfo(requestType) {
     console.log("get form info");
-    var endpoint = "/user/all/";
+    var endpoint = "/user/";
 
     var data = {}
     if (requestType == "GET"){
         var username = document.getElementById("login-username").value;
         var password = document.getElementById("login-password").value;
-        data = {username: password};
-        makeNetworkCalltoServer(hostUrl, portNumber, requestType, endpoint, data);
+        data = {'username': username, 'password': password};
+        makeNetworkCalltoServer(hostUrl, portNumber, requestType, endpoint + 'all/', data);
     } else if (requestType == "POST"){
         var fname = document.getElementById("signup-fname").value;
         var lname = document.getElementById("signup-lname").value;
@@ -63,10 +63,10 @@ function handleRequest(hostUrl, portNumber, requestType, endpoint, data) {
         console.log(xhr.responseText);
 
         if (requestType == "GET"){
-            updateGET(xhr.responseText);
+            updateGET(xhr.responseText, data);
         } else if (requestType == "POST"){
             updatePOST(xhr.responseText);
-        } else if (requestType == "DELETE"){
+        }else if (requestType == "DELETE"){
             updateDELETE(xhr.responseText);
         }
     }
@@ -81,4 +81,42 @@ function handleRequest(hostUrl, portNumber, requestType, endpoint, data) {
     } else if (requestType == "POST" || requestType == "DELETE"){
         xhr.send(JSON.stringify(data));
     }
+}
+
+function updateGET(response_text, data){
+    var response_json = JSON.parse(response_text);
+
+    if (response_json[data['username']]){
+        console.log("match")
+        // user exists
+        if (response_json[data['username']]['password'] == data['password']){
+            document.getElementById("head-username").innerHTML = data['username'];
+            document.getElementById("login").style.display = "none";
+            document.getElementById("signup").style.display = "none";
+            document.getElementById("delete").style.display = "inline";
+            document.getElementById("greeting").innerHTML = "Hello, " + data['username'] + ".";
+        } else {
+            alert("Incorrect password")
+        }
+    } else {
+        // no user exists
+        console.log("no match")
+        document.getElementById("head-username").innerHTML = "";
+        document.getElementById("login").style.display = "none";
+        document.getElementById("signup").style.display = "inline";
+        document.getElementById("delete").style.display = "none";
+    }
+
+}
+
+function updatePOST(response_text){
+    document.getElementById("login").style.display = "inline";
+    document.getElementById("signup").style.display = "none";
+    document.getElementById("delete").style.display = "none";
+}
+
+function updateDELETE(response_text){
+    document.getElementById("login").style.display = "inline";
+    document.getElementById("signup").style.display = "none";
+    document.getElementById("delete").style.display = "none";
 }
